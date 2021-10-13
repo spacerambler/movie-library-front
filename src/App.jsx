9a3d4a49c1601d21bedb376781f2eae124a1a734
Keely Bailey <keelybailey3@gmail.com>
2021-10-12 21:09:11 -0500

    Clean up files. Update homeview

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import { ApolloClient, ApolloProvider } from "@apollo/client";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AuthContext from "./context/AuthContext";
+import UserService from "./services/User"
 
 import "./App.css";
 import HomeView from "./pages/Movies";
 
-import getMovies from "./services/API.services";
-import MovieCardList from "./components/MovieCardList";
-
 // const client = new ApolloClient({
-//   url: "http://localhost:4000/graphql",
+//   uri: "http://localhost:4000/graphql",
 // });
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    console.log("use effect running")
-    getMovies().then((jsonData) => {
-      console.log("use effect" + jsonData)
-      setData(jsonData);
-    })
-  }, []);
 
   return (
-    <main>
-    <Router>
-    {/* <ApolloProvider client={client}> ... not sure how to use this right now yet */}
-      <Route path="/" exact>
-        <HomeView />
-      </Route>
-    </Router>
-    </main>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          {/* Provide a way to access and/or set the current user. */}
+          <AuthContext.Provider value={React.useState(UserService.getUser())}>
+            <Route path={["/signup", "/login"]}>
+              <AddUserLoginView />
+            </Route>
+            <Route exact path="/">
+              <HomeView />
+            </Route>
+          </AuthContext.Provider>
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
