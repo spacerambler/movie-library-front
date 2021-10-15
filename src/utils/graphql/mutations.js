1ff456366e7578e39fe4f511f9cb89f381b21ed5
Keely Bailey <keelybailey3@gmail.com>
2021-10-15 11:03:11 -0500

    Update schema to mutations

---
+++ src/utils/graphql/mutations.js
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+mutation addUser(
+  $name: String!
+  $email: String!
+  $password: String!
+) {
+  addUser(
+    name: $name
+    email: $email
+    password: $password
+  ) {
+    token
+    user {
+      _id
+      name
+    }
+  }
+}`;
+
+export const LOGIN = gql`
+mutation Login($email: String!, $password: String!) {
+login(email: $email, password: $password) {
+  token
+  user {
+    name
+    _id
+  }
+}
+}
+`;
